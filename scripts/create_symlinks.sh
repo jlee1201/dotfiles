687584f453f44c2fbe56925bee1bb0e7051d4b60
John Lee <jlee1201@users.noreply.github.com>
2021-03-12 13:12:04 -0500

    create the symlinks so I can live code changes into the appropriate places on my machine

---
+++ scripts/create_symlinks.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
+
+ln -sf "$DIR"/.zshrc ~/.zshrc
+ln -sf "$DIR"/.gitconfig ~/.gitconfig
+ln -sfn "$DIR"/.zfunc ~/.zfunc
+ln -sf "$DIR"/.ssh/config ~/.ssh/config
+ln -sf "$DIR"/.config/karabiner.edn ~/.config/karabiner.edn
+
