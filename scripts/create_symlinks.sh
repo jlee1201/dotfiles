81605b34c03e0bc4bc78c151943db10bbc59dba5
John Lee <jlee1201@users.noreply.github.com>
2021-10-04 12:59:19 -0400

    - up arrow movement_mode - added .rvmrc to suppress rvm message (zsh quiet instant prompt) - something updated .gitconfig

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -2,6 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 
 ln -sf "$DIR"/.zshrc ~/.zshrc
+ln -sf "$DIR"/.rvmrc ~/.rvmrc
 ln -sf "$DIR"/.p10k.zsh ~/.p10k.zsh
 ln -sf "$DIR"/.gitconfig ~/.gitconfig
 ln -sfn "$DIR"/.zfunc ~/.zfunc
