88529a9656428bf20392183957fc360e3c367fa9
John Lee <jlee1201@users.noreply.github.com>
2022-07-15 14:21:35 -0400

    - add yarn alias - add keyboard repeat rate script - fix some weirdness around .profile vs .bash_profile

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -3,6 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 
 ln -sf "$DIR"/.zshrc ~/.zshrc
 ln -sf "$DIR"/.profile ~/.profile
+ln -sf "$DIR"/.profile ~/.bash_profile
 ln -sf "$DIR"/.rvmrc ~/.rvmrc
 ln -sf "$DIR"/.p10k.zsh ~/.p10k.zsh
 ln -sf "$DIR"/.gitconfig ~/.gitconfig
---
+++ scripts/keyboard_repeat_rate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+defaults write -g InitialKeyRepeat -int 13 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
