b2d212d9dcb9f8b4af1db778f8898315ee65b879
John Lee <jlee1201@users.noreply.github.com>
2024-05-01 13:14:05 -0400

    * add reminder about keyboard repeat rate script

--- scripts/keyboard_repeat_rate.sh
+++ scripts/keyboard_repeat_rate.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 defaults write -g InitialKeyRepeat -int 13 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+
+# reminder -- need to re-login to macbook to see results
+ˆ
