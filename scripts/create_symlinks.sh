8be578930ee5d458c2bd09329eeede3ef4ef5489
John Lee <jlee1201@users.noreply.github.com>
2024-07-15 17:22:16 -0400

    config changes - refactor script

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 
-ln -sf "$DIR"/.zshrc ~/.zshrc
-ln -sf "$DIR"/.profile ~/.profile
-ln -sf "$DIR"/.profile ~/.bash_profile
-ln -sf "$DIR"/.p10k.zsh ~/.p10k.zsh
-ln -sf "$DIR"/.gitconfig ~/.gitconfig
-ln -sfn "$DIR"/.zfunc ~/.zfunc
-ln -sf "$DIR"/.ssh/config ~/.ssh/config
+# symlinked nested
 ln -sf "$DIR"/.config/karabiner.edn ~/.config/karabiner.edn
 ln -sfn "$DIR"/.hammerspoon ~/.hammerspoon
+ln -sf "$DIR"/.ssh/config ~/.ssh/config
+ln -sfn "$DIR"/.zfunc ~/.zfunc
 ln -sf "$DIR"/nvm/default-packages "$NVM_DIR"/default-packages
+
+# symlinked files
+ln -sf "$DIR"/.gitconfig ~/.gitconfig
+ln -sf "$DIR"/.gitignore ~/.gitignore
+ln -sf "$DIR"/.p10k.zsh ~/.p10k.zsh
+ln -sf "$DIR"/.profile ~/.profile
+ln -sf "$DIR"/.profile ~/.bash_profile
+ln -sf "$DIR"/.ruby-version ~/.ruby-version
+ln -sf "$DIR"/.zshrc ~/.zshrc
+
+
